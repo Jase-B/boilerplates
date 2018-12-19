13919b7a01da6919fe7a16f5211fcf8762f203c8
Jase Balderrama <jasebalderrama@icloud.com>
2018-12-18 18:02:15 -0700

    Swap Vue for React

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-console.log('yup...')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  React.createElement(App),
+  document.querySelector('#app')
+);
