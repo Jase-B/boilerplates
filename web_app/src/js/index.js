4a5577c38a2df7459968bfc43004af2cabfcb4a8
Jase Balderrama <jasebalderrama@icloud.com>
2019-08-06 11:35:51 -0700

    Swap 'import' for 'require' in React index

--- web_app/src/js/index.js
+++ web_app/src/js/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./components/App');
 
 ReactDOM.render(
   React.createElement(App),
